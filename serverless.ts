2d758a95ecff5a01de1e4a59df2abda277082600
cesar_ciau <cesarfhs@gmail.com>
2022-01-14 00:56:38 -0500

    initial commit

---
+++ serverless.ts
@@ -0,0 +1,59 @@
+import type { AWS } from '@serverless/typescript';
+
+import hello from '@functions/hello';
+
+const serverlessConfiguration: AWS = {
+  service: 'aws-lambda',
+  frameworkVersion: '2',
+  plugins: ['serverless-esbuild', 'serverless-offline', 'serverless-dynamodb-local'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  // import the function via paths
+  functions: { hello },
+  package: { individually: true },
+  custom: {
+    dynamodb: {
+      stages: ['dev'],
+      start: {
+        port: 8000,
+        inMemory: true,
+        heapInitial: '200m',
+        heapMax: '1g',
+        migrate: true,
+        seed: true,
+        convertEmptyValues: true,
+        // Uncomment only if you already have a DynamoDB running locally
+        // noStart: true
+      }
+    },
+    ['serverless-offline']: {
+      httpPort: 3000,
+      babelOptions: {
+        presets: ["env"]
+      }
+    },
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
